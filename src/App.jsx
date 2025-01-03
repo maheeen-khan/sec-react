3e0cf510d7c550ec9180666bef231298e150fc03
maheeen-khan <maheeenkhan4@gmail.com>
2025-01-04 00:37:07 +0500

    updated

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+    <h1>hi</h1>
+    <Navbar/>
+    </>
+  );
+}
+
+export default App;
