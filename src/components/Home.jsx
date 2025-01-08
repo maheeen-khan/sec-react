0ffa651a4fcf97bed3e835dcaa74bc570b65ca82
maheeen-khan <maheeenkhan4@gmail.com>
2025-01-08 20:07:49 +0500

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,34 +1,36 @@
 import React from 'react'
 import { Col, Row, Divider } from 'antd';
 import traveller from '../assets/Traveller 1.png';
-import play from '../assets//Play button.png';
+import play from '../assets/Play button.png';
+import plane from '../assets/plane.png'
 
 import './style.css'
 const Home = () => {
     return (
         <>
             {/* <Divider orientation="left">Percentage columns</Divider> */}
-            <Row className='align-content-center my-3'>
+            <Row className='my-3'>
 
-                <Col span={10} className='px-3 mt-3 homeImg' xs={24} sm={24} md={12} lg={12} xl={12}>
+                <Col span={10} className='px-2 mt-3 homeImg' xs={24} sm={24} md={12} lg={12} xl={12}>
+
+                <img src={plane} alt=""/>
                     <img src={traveller} alt="" className='img-fluid' width={370} />
                 </Col>
 
 
-                <Col span={10} className='p-5 homeCont' xs={24} sm={24} md={12} lg={12} xl={12}>
+                <Col span={10} className='p-lg-5 p-md-5 mt-3 homeCont' xs={24} sm={24} md={12} lg={12} xl={12}>
                     <h6 style={{ textTransform: 'uppercase', color: '#DF6951' }} className='mb-3'>Best Destinations around the world</h6>
-                    <h1 className="head mb-4" style={{ fontWeight: '500' }}>Travel, enjoy
+                    <h1 className="head mb-4" style={{ fontWeight: '500', color:'rgb(124, 124, 124)' }}>Travel, enjoy
                         and live a new
                         and full life</h1>
 
                     <p style={{ color: '#5E6282' }} className='mb-4'>Built Wicket longer admire do barton vanity itself do in it. Preferred to sportsmen it engrossed listening. Park gate sell they west hard for the.</p>
 
-                    <div className="both-front">
+                    <div className="both-front d-flex align-items-center flex-wrap-reverse">
                         <button className='btn findBtn py-2'>Find out more</button>
 
                         <button className='btn' style={{ color: 'rgb(82, 81, 81)' }}>
-                            <img src={play} alt="" width={70} className='playBtn' />
-                            Play Demo
+                            <img src={play} alt="" width={70} className='playBtn' />Play Demo
                         </button>
                     </div>
                 </Col>
