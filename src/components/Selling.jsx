084a9bf8cc848f3578a401a9462ed2911c35a4d7
maheeen-khan <maheeenkhan4@gmail.com>
2025-01-11 16:31:17 +0500

    updated

--- src/components/Selling.jsx
+++ src/components/Selling.jsx
@@ -1,19 +1,41 @@
 import { Col, Row } from 'antd'
 import React from 'react'
 import ThirdCard from './innerComponents/ThirdCard'
+import img1 from '../assets/Third1.png'
+import img2 from '../assets/Third2.jpg'
+import img3 from '../assets/Third3.png'
 
 const Selling = () => {
     return (
         <>
-            <div className="container">
+            <div className="container text-center pt-4">
 
                 <p>Top Selling</p>
-                <h2>Top Destinations</h2>
+                <h2 className='mb-5 third-head'>Top Destinations</h2>
 
-                <Row>
+                <Row className='gap-4'>
                     <Col>
 
-                     <ThirdCard/>
+                        <ThirdCard img={img1} name={'Rome, Italty'} price={'$5,42k'} days={'10 Days Trip'} />
+
+                    </Col>
+
+                    <Col>
+
+                        <ThirdCard img={img2} name={'London, UK'} price={'$4.2k'} days={'12 Days Trip'} />
+
+                    </Col>
+
+                    <Col>
+
+                        <ThirdCard img={img3} name={'Full Europe'} price={'$15k'} days={'28 Days Trip'} />
+
+                    </Col>
+
+                    <Col>
+
+                        <ThirdCard img={img2} name={'London, UK'} price={'$4.2k'} days={'12 Days Trip'} />
+
                     </Col>
                 </Row>
             </div>
--- src/components/innerComponents/ThirdCard.jsx
+++ src/components/innerComponents/ThirdCard.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import { Avatar, Card, Col, Row } from 'antd';
-import img1 from '../../assets/Third1.png'
 import nav from '../../assets/nav.png'
 const { Meta } = Card;
 
-const ThirdCard = () => (
+const ThirdCard = (props) => (
   <Card 
   className='thir-car'
     style={{
@@ -14,7 +13,7 @@ const ThirdCard = () => (
     cover={
       <img
         alt="example"
-        src={img1}
+        src={props.img}
       />
     }
 
@@ -22,17 +21,17 @@ const ThirdCard = () => (
 
     <Row className='d-flex justify-content-between mb-2'>
         <Col>
-        <h6 style={{fontSize:'14px', color:'rgb(72, 72, 72)'}}>Rome, Italty</h6>
+        <h6 style={{fontSize:'14px', color:'rgb(72, 72, 72)'}}>{props.name}</h6>
         </Col>
         <Col>
-        <h6  style={{fontSize:'14px', color:'rgb(72, 72, 72)'}}>$5,42k</h6>
+        <h6  style={{fontSize:'14px', color:'rgb(72, 72, 72)'}}>{props.price}</h6>
         </Col>
     </Row>
     <Row>
       
         <Col style={{fontSize:'11px'}}>
         <p className='navImg'><img src={nav} alt="" className='me-2'/>
-        10 Days Trip</p>
+        {props.days}</p>
         </Col>
     </Row>
 
