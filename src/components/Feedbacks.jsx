3d68a595907895bbbd69ae6a5e0f78aeefc571c8
maheeen-khan <maheeenkhan4@gmail.com>
2025-01-13 23:22:58 +0500

    updated

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -5,17 +5,25 @@ import FifthCard from './innerComponents/FifthCard'
 const Feedbacks = () => {
   return (
     <>
-    <Row >
+    <div className="py-5">
+    <Row justify="space-around" align="middle" className="d-flex align-items-center">
         <Col sm={24} md={24} lg={8}>
-        <h6 style={{color:'grey'}}>Testimonials</h6>
-        <h2 style={{fontFamily:'hi'}}>What people say about Us.</h2>
+        <h6 style={{color:'#31ade2'}} className='mb-3'>Testimonials</h6>
+        <h2 style={{fontFamily:'hi'}} className='mb-4'>What people say about Us.</h2>
+        <p style={{color:'grey'}}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo quos nesciunt labore sit, quae repellendus maxime hic id illo et.</p>
         </Col>
 
-        <Col>
+        <Col className='d-flex'>
+       
+        <div className="col1">
         <FifthCard/>
+        </div>
+        <FifthCard/>
+        
         </Col>
 
     </Row>
+    </div>
     </>
   )
 }
